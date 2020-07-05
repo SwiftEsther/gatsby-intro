09f4659fd6b2c5ded31040aa3b1e74d86764de23
Esther Akinloose <esther.akinloose@carpware.co>
2020-07-05 03:10:26 +0100

    Set up site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports={
+    siteMetadata: {
+        title: 'Frontend Masters Gatsby Workshop',
+        description: 'A site I built while following the Frontend Masters Gatsby Workshop',
+    },
     plugins: ['gatsby-plugin-emotion'],
 };
