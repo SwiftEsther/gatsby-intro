f8c3293d06fb7bda136411c03bc9f324bc4a252d
Esther Akinloose <esther.akinloose@carpware.co>
2020-07-05 05:10:22 +0100

    created the usePosts hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
 
-export default () => (
-    <Layout>
-        <h1>Home</h1>
-        <p>Hello Nigeria!</p>
-        <Link to='/about/'>&rarr; Learn more about me</Link>
-    </Layout>
-);
+export default () => {
+    const posts = usePosts();
+    return(
+        <Layout>
+            <h1>Home</h1>
+            <p>Hello Nigeria!</p>
+            <Link to='/about/'>&rarr; Learn more about me</Link>
+
+            <h2>Read my blog</h2>
+            {posts.map(post => (
+                <pre>{JSON.stringify(post, null, 2)}</pre>
+            ))}
+        </Layout>
+    )
+};
